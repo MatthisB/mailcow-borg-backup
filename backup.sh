230d838613535cc81af2879bec19e92e3304c22a
MatthisB <github@matthis-brugger.de>
2020-11-06 17:16:13 +0100

    add backup.sh
    initial upload

---
+++ backup.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+###############################
+#                             #
+#     Mailcow Borg Backup     #
+#                             #
+###############################
+# Author:       Matthis B.    #
+# Created:      20201105      #
+# Lastchange:   20201105      #
+###############################
+# Changelog:                  #
+# - 20201105: init            #
+###############################
+
+##
+## Cronjob:
+## 7 4 * * * /home/path/to/backup.sh > /home/path/to/backup/log/mailcow_sys_$(date +\%Y-\%m-\%d-\%H-\%M-\%S).log 2>&1
+##
+
+#
+# Functions
+#
+getDate() { echo $(date +'%d.%m.%Y %H:%M:%S'); }
+info() { printf "\n- %s %s\n\n" "$( getDate )" "$*" >&2; }
+trap "echo $( getDate ) Backup interrupted >&2; exit 2" INT TERM
+
+
+#
+# Settings
+#
+
+# System vars
+workDirectory='/opt/mailcow-dockerized'
+
+logDirectory='/home/path/to/backup/log'
+logKeepAmount='10'
+
+
+# Borg env vars
+BORG_PREFIX='mx10'
+
+export BORG_REPO='ssh://borguser@123.123.123.123:22/repo/path'
+export BORG_PASSPHRASE='passw0rd'
+
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
+
+# autocatch vars
+source "${workDirectory}/mailcow.conf"
+CMPS_PRJ=$(echo $COMPOSE_PROJECT_NAME | tr -cd "[A-Za-z-_]")
+
+volumeVMail=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_vmail-vol-1)
+volumeCrypt=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_crypt-vol-1)
+volumeRedis=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_redis-vol-1)
+volumeRSpamd=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_rspamd-vol-1)
+volumePostfix=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_postfix-vol-1)
+volumeMySQL=$(docker volume inspect -f '{{ .Mountpoint }}' ${CMPS_PRJ}_mysql-vol-1)
+
+
+
+#
+# Prerequisites
+#
+
+# check for root
+if [[ "$(id -u)" != "0" ]] ; then
+  info "ERROR: no root!"
+  exit 1
+fi
+
+# check project name
+if [[ -z "${CMPS_PRJ}" ]] ; then
+  info "ERROR: empty docker-project-name"
+  exit 1
+fi
+
+# create directories if not already done
+if [[ ! -d "${logDirectory}" ]] ; then
+  if ! mkdir -p "${logDirectory}" ; then
+    info "ERROR: could not create logDirectory ($logDirectory)"
+    exit 1
+  fi
+fi
+
+
+
+#
+# Pre-BackupProcess
+#
+startTime=$(date +%s)
+
+echo
+echo "=================================================="
+info "start backup"
+
+
+# clean old stuff
+echo "-- clean up temp/log"
+countLogs=$(ls -l ${logDirectory}/*.log 2>/dev/null | wc -l)
+echo "--- found $countLogs old log files"
+if (( "$countLogs" >= "$logKeepAmount" )) ; then
+  echo "--- delete old logfiles up to the last $logKeepAmount .."
+  ls -dt "${logDirectory}"/*".log" | tail -n "+$((logKeepAmount+1))" | xargs rm -v
+fi
+
+echo
+echo "-- pre borg stuff"
+
+# ensure no more changes are made (e.g. send/receive mails, WebUI changes)
+echo "--- mailcow: stop services"
+echo "---- stop dovecot"
+dovecot_id=$(docker stop "$(docker ps -qf name=dovecot-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.ExitCode }}' "$dovecot_id")" == "0" && "$(docker inspect -f '{{ .State.Running }}' "$dovecot_id")" == "false" ]] ; then
+  echo "----- success"
+else
+  echo "----- FAILED"
+  exit 1
+fi
+echo "---- stop postfix"
+postfix_id=$(docker stop "$(docker ps -qf name=postfix-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.ExitCode }}' "$postfix_id")" == "0" && "$(docker inspect -f '{{ .State.Running }}' "$postfix_id")" == "false" ]] ; then
+  echo "----- success"
+else
+  echo "----- FAILED"
+  exit 1
+fi
+
+# show error on webstuff
+echo "--- nginx return service unavailable"
+echo "return 503;" > "${workDirectory}/data/conf/nginx/site.backup.custom"
+nginx_id=$(docker restart "$(docker ps -qf name=nginx-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.Running }}' "$nginx_id")" == "true" ]] ; then
+  echo "---- success"
+else
+  echo "---- FAILED"
+  exit 1
+fi
+
+# backup database: mysql
+echo "--- mysql-dump"
+if [[ -d "${volumeMySQL}/tmp_backup" ]] ; then
+  echo "---- WARN: tmp backup dir already exists"
+  rm -r "${volumeMySQL}/tmp_backup"
+  if [[ -d "${volumeMySQL}/tmp_backup" ]] ; then
+    echo "---- FAILED: could not delete"
+    exit 1
+  fi
+fi
+mysql_id=$(docker ps -qf 'name=mysql-mailcow')
+docker exec ${mysql_id} /bin/sh -c "mariabackup --host mysql --user root --password ${DBROOT} \
+                                        --backup --rsync --target-dir=/var/lib/mysql/tmp_backup ; \
+									mariabackup --prepare --target-dir=/var/lib/mysql/tmp_backup ; \
+									chown -R 999:999 /var/lib/mysql/tmp_backup ;" > /dev/null 2>&1
+if [[ -d "${volumeMySQL}/tmp_backup" ]] ; then
+  echo "---- success"
+else
+  echo "---- FAILED: could not create backup"
+  exit 1
+fi
+
+# backup database: redis
+echo "--- redis-dump"
+redisdump=$(docker exec $(docker ps -qf name=redis-mailcow) redis-cli save)
+if [[ "$redisdump" == "OK" ]]; then
+  echo "---- success"
+else
+  echo "---- FAILED: $redisdump"
+  exit 1
+fi
+
+
+
+#
+# BackupProcess
+#
+
+# starting borg
+echo
+echo "-- start syncing files"
+echo
+
+borg create															\
+  --show-rc															\
+  --verbose															\
+  --stats															\
+  --compression lz4													\
+  --exclude-caches													\
+  ::"${BORG_PREFIX}-{now:%Y-%m-%d_%H:%M:%S}"						\
+  "${workDirectory}/.env"											\
+  "${workDirectory}/docker-compose.yml"								\
+  "${workDirectory}/mailcow.conf"									\
+  "${volumeVMail}"													\
+  "${volumeCrypt}"													\
+  "${volumeRedis}"													\
+  "${volumeRSpamd}"													\
+  "${volumePostfix}"												\
+  "${volumeMySQL}/tmp_backup"										\
+  "${workDirectory}/data/conf/nginx/redirect.conf"						# additional
+
+borg_create_exit=$?
+
+echo
+
+# check state
+echo
+if [[ "${borg_create_exit}" == "0" ]] ; then
+  info "borg finished successfully"
+elif [[ "${borg_create_exit}" == "1" ]] ; then
+  info "borg finished with warnings"
+else
+  info "borg finished with errors"
+fi
+# calc backup duration
+endTime=$(date +%s)
+duration=$((endTime-startTime))
+echo "-- backup duration: $(printf '%02d hours %02d minutes %02d seconds' $((duration / 3600)) $(((duration / 60) % 60)) $((duration % 60)))"
+
+
+
+#
+# Post-BackupProcess
+#
+
+echo
+echo "-- post borg stuff"
+
+echo "--- mailcow: re/start services"
+echo "---- start dovecot"
+dovecot_id=$(docker start "$(docker ps -aqf name=dovecot-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.Running }}' "$dovecot_id")" == "true" ]] ; then
+  echo "----- success"
+else
+  echo "----- FAILED"
+  exit 1
+fi
+echo "---- start postfix"
+postfix_id=$(docker start "$(docker ps -aqf name=postfix-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.Running }}' "$postfix_id")" == "true" ]] ; then
+  echo "----- success"
+else
+  echo "----- FAILED"
+  exit 1
+fi
+
+# show error on webstuff
+echo "--- nginx normal mode"
+if [[ -f "${workDirectory}/data/conf/nginx/site.backup.custom" ]] ; then
+  rm "${workDirectory}/data/conf/nginx/site.backup.custom"
+fi
+nginx_id=$(docker restart "$(docker ps -qf name=nginx-mailcow)")
+if [[ "$(docker inspect -f '{{ .State.Running }}' "$nginx_id")" == "true" ]] ; then
+  echo "---- success"
+else
+  echo "---- FAILED"
+  exit 1
+fi
+
+# clean tmp stuff
+echo "--- clean up"
+
+if [[ -d "${volumeMySQL}/tmp_backup" ]] ; then
+  rm -r "${volumeMySQL}/tmp_backup"
+  echo "---- removed MySQL tmp_backup dir"
+fi
+
+
+
+info "-> everything done: exit"
+
+exit ${borg_create_exit}
+
